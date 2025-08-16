8ec90c53cf4f342707e58d43c232ed86a511ebcd
AnakinRaW <anakin@gmx-topmail.de>
2025-08-16 18:29:44 +0200

    make marker static

--- src/ModVerify.CliApp/Resources/Baselines/BaselineResources.cs
+++ src/ModVerify.CliApp/Resources/Baselines/BaselineResources.cs
@@ -1,4 +1,4 @@
 ﻿namespace AET.ModVerify.App.Resources.Baselines;
 
 // Marker class to provide static namespace information for resource lookup.
-internal class BaselineResources;
+internal static class BaselineResources;
