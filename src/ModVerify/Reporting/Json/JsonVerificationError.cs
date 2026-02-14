bc8d3b173e826d5161b5e28ffa7dfddfa6aafe2f
AnakinRaW <anakin@gmx-topmail.de>
2026-02-14 12:17:33 +0100

    move to files

--- src/ModVerify/Reporting/Json/JsonVerificationError.cs
+++ src/ModVerify/Reporting/Json/JsonVerificationError.cs
@@ -4,50 +4,6 @@ using System.Text.Json.Serialization;
 
 namespace AET.ModVerify.Reporting.Json;
 
-[JsonDerivedType(typeof(JsonVerificationError))]
-[JsonDerivedType(typeof(JsonAggregatedVerificationError))]
-internal abstract class JsonVerificationErrorBase
-{
-    [JsonPropertyName("id")]
-    public string Id { get; }
-
-    [JsonPropertyName("verifiers")]
-    public IReadOnlyList<string> VerifierChain { get; }
-
-    [JsonPropertyName("message")]
-    public string Message { get; }
-
-    [JsonPropertyName("severity")]
-    [JsonConverter(typeof(JsonStringEnumConverter<VerificationSeverity>))]
-    public VerificationSeverity Severity { get; }
-
-    [JsonPropertyName("asset")]
-    public string Asset { get; }
-
-    protected JsonVerificationErrorBase(
-        string id,
-        IReadOnlyList<string>? verifierChain,
-        string message,
-        VerificationSeverity severity,
-        string? asset)
-    {
-        Id = id;
-        VerifierChain = verifierChain ?? [];
-        Message = message;
-        Severity = severity;
-        Asset = asset ?? string.Empty;
-    }
-
-    protected JsonVerificationErrorBase(VerificationError error)
-    {
-        Id = error.Id;
-        VerifierChain = error.VerifierChain.Select(x => x.Name).ToList();
-        Message = error.Message;
-        Severity = error.Severity;
-        Asset = error.Asset;
-    }
-}
-
 internal class JsonVerificationError : JsonVerificationErrorBase
 {
     [JsonPropertyName("context")]
@@ -69,31 +25,5 @@ internal class JsonVerificationError : JsonVerificationErrorBase
     public JsonVerificationError(VerificationError error) : base(error)
     {
         ContextEntries = error.ContextEntries.Any() ? error.ContextEntries : null;
-    }
-}
-
-internal class JsonAggregatedVerificationError : JsonVerificationErrorBase
-{
-    [JsonPropertyName("contexts")]
-    [JsonPropertyOrder(99)]
-    public IEnumerable<IEnumerable<string>> Contexts { get; }
-
-    [JsonConstructor]
-    public JsonAggregatedVerificationError(
-        string id,
-        IReadOnlyList<string>? verifierChain,
-        string message,
-        VerificationSeverity severity,
-        IEnumerable<IEnumerable<string>>? contexts,
-        string? asset) : base(id, verifierChain, message, severity, asset)
-    {
-        Contexts = contexts ?? [];
-    }
-
-    public JsonAggregatedVerificationError(
-        VerificationError error,
-        IEnumerable<IEnumerable<string>> contexts) : base(error)
-    {
-        Contexts = contexts;
     }
 }
---
+++ src/ModVerify/Reporting/Json/JsonVerificationErrorBase.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+
+namespace AET.ModVerify.Reporting.Json;
+
+[JsonDerivedType(typeof(JsonVerificationError))]
+[JsonDerivedType(typeof(JsonAggregatedVerificationError))]
+internal abstract class JsonVerificationErrorBase
+{
+    [JsonPropertyName("id")]
+    public string Id { get; }
+
+    [JsonPropertyName("verifiers")]
+    public IReadOnlyList<string> VerifierChain { get; }
+
+    [JsonPropertyName("message")]
+    public string Message { get; }
+
+    [JsonPropertyName("severity")]
+    [JsonConverter(typeof(JsonStringEnumConverter<VerificationSeverity>))]
+    public VerificationSeverity Severity { get; }
+
+    [JsonPropertyName("asset")]
+    public string Asset { get; }
+
+    protected JsonVerificationErrorBase(
+        string id,
+        IReadOnlyList<string>? verifierChain,
+        string message,
+        VerificationSeverity severity,
+        string? asset)
+    {
+        Id = id;
+        VerifierChain = verifierChain ?? [];
+        Message = message;
+        Severity = severity;
+        Asset = asset ?? string.Empty;
+    }
+
+    protected JsonVerificationErrorBase(VerificationError error)
+    {
+        Id = error.Id;
+        VerifierChain = error.VerifierChain.Select(x => x.Name).ToList();
+        Message = error.Message;
+        Severity = error.Severity;
+        Asset = error.Asset;
+    }
+}
