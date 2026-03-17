c6290ced0d536c74517289aedf9b523f5dce6daa
AnakinRaW <anakin@gmx-topmail.de>
2026-03-17 16:20:33 +0100

    hardcoded assets verifier

--- src/ModVerify/DefaultGameVerifiersProvider.cs
+++ src/ModVerify/DefaultGameVerifiersProvider.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using AET.ModVerify.Settings;
+﻿using AET.ModVerify.Settings;
 using AET.ModVerify.Verifiers;
 using AET.ModVerify.Verifiers.CommandBar;
+using AET.ModVerify.Verifiers.Engine;
 using AET.ModVerify.Verifiers.GameObjects;
 using AET.ModVerify.Verifiers.GuiDialogs;
 using AET.ModVerify.Verifiers.SfxEvents;
 using PG.StarWarsGame.Engine;
+using System;
+using System.Collections.Generic;
 
 namespace AET.ModVerify;
 
@@ -19,6 +20,7 @@ public sealed class DefaultGameVerifiersProvider : IGameVerifiersProvider
     {
         //yield break;
         yield return new SfxEventVerifier(gameEngine, settings, serviceProvider);
+        yield return new HardcodedAssetsVerifier(gameEngine, settings, serviceProvider);
         yield return new GuiDialogsVerifier(gameEngine, settings, serviceProvider);
         yield return new GameObjectTypeVerifier(gameEngine, settings, serviceProvider);
         yield return new CommandBarVerifier(gameEngine, settings, serviceProvider);
---
+++ src/ModVerify/Verifiers/Engine/HardcodedAssetsVerifier.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using AET.ModVerify.Settings;
+using AET.ModVerify.Verifiers.Commons;
+using PG.StarWarsGame.Engine;
+
+namespace AET.ModVerify.Verifiers.Engine;
+
+public sealed class HardcodedAssetsVerifier : GameVerifier
+{
+    private readonly SingleModelVerifier _modelVerifier;
+
+    public HardcodedAssetsVerifier(IStarWarsGameEngine gameEngine, GameVerifySettings settings, IServiceProvider serviceProvider) 
+        : base(gameEngine, settings, serviceProvider)
+    {
+        _modelVerifier = new SingleModelVerifier(this);
+    }
+
+    public override void Verify(CancellationToken token)
+    {
+        OnProgress(0.0d, "Verifying Hardcoded Models");
+        VerifyModels(token);
+        OnProgress(1.0, null);
+    }
+
+    private void VerifyModels(CancellationToken token)
+    {
+        var models = HardcodedEngineAssets.GetHardcodedModels(GameEngine.EngineType);
+
+        foreach (var model in models) 
+            _modelVerifier.Verify(model, [], token);
+
+        foreach (var error in _modelVerifier.VerifyErrors) 
+            AddError(error);
+    }
+}
